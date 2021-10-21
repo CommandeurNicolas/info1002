f9e90f72edc67f781b10278abcf15708ef57594c
Nicolas.C <comcomnico@gmail.com>
2021-10-21 15:56:25 +0200

    adding more commands

--- Inverter.cpp
+++ Inverter.cpp
@@ -7,41 +7,66 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <random>
+#include <limits>
+#include <bitset>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <iostream>
 
 using namespace std;
 
-Inverter::Inverter(string alphabet, long min, long max, void hashingMethod(string, Byte *)) {
+Inverter::Inverter(){}
+
+Inverter::Inverter(string alphabet, uint64_t min, uint64_t max, void hashMethod(string, Byte *), int sizeByte,
+                   string hashingMethod) {
     this->alphabet = alphabet;
 
     this->minSize = min;
     this->maxSize = max;
-    this->hash = hashingMethod;
+    this->hash = hashMethod;
+    this->hashingMethod = hashingMethod;
     this->N = 0;
-    this->size = (long) this->alphabet.length();
-    this->powT = {};
+    this->size = (uint64_t)
+    this->alphabet.length();
 
-    for (long i = this->minSize; i <= this->maxSize; i++) {
+    this->sizeByte = sizeByte;
+    this->b = new Byte[this->sizeByte];
+    this->texte = new char[this->maxSize];
+
+    this->chaine.b = 0;
+    this->chaine.e = 0;
+
+    this->table.hauteur = 0;
+    this->table.largeur = 0;
+
+    this->powT = {};
+    for (uint64_t i = this->minSize; i <= this->maxSize; i++) {
         this->N += pow(this->size, i);
-        this->powT.push_back(pow((long) this->size, i));
+        this->powT.push_back(pow((uint64_t)
+        this->size, i));
     }
 
 }
 
-string Inverter::i2c(long code) {
-    long codeTemp = code;
+string Inverter::i2c(uint64_t code) {
+    uint64_t codeTemp = code;
     string res = "";
     int n = 0;
-
-    for (int i = this->minSize; i <= this->maxSize; i++) {
+    int i;
+    for (i = this->minSize; i <= this->maxSize; i++) {
         if (this->powT[n] < codeTemp) {
             codeTemp -= this->powT[n];
             n++;
+        } else {
+            break;
         }
     }
 
     char letter = codeToLetter(codeTemp % this->size);
 
-    for (int i = 0; i < this->maxSize; i++) {
+    for (int j = 0; j < i; j++) {
         res.append(string(1, letter));
         codeTemp = codeTemp / this->size;
         letter = codeToLetter(codeTemp % this->size);
@@ -51,15 +76,121 @@ string Inverter::i2c(long code) {
     return res;
 }
 
-char Inverter::codeToLetter(long code) {
-
+char Inverter::codeToLetter(uint64_t code) {
     return this->alphabet[code];
-
 }
 
 // t begin at 0 instead of 1
 uint64_t Inverter::h2i(Byte *empreinte, int t) {
     Byte *temp = empreinte;
-    return *((uint64_t *) temp + t) % this->N;
+    return (*((uint64_t *) temp) + t) % this->N;
 }
 
+uint64_t Inverter::i2i(uint64_t code, int indice) {
+    this->hash(i2c(code), this->b);
+    return h2i(this->b, indice);
+}
+
+Chaine Inverter::nouvelle_chaine(uint64_t idxl, int largeur) {
+    Chaine chaine;
+    chaine.b = idxl;
+    uint64_t code = idxl;
+    for (int i = 1; i < largeur; ++i) {
+        code = i2i(code, i);
+    }
+    chaine.e = code;
+    return chaine;
+}
+
+uint64_t Inverter::index_aleatoire() {
+    random_device rd;
+    mt19937_64 eng(rd());
+    uniform_int_distribution <uint64_t> distr;
+    return (distr(eng)) % this->N;
+}
+
+bool compare2Chaine(const Chaine &a, const Chaine &b) {
+    return a.e < b.e;
+}
+
+Table Inverter::creer_table(int largeur, int hauteur) {
+    Table t;
+    t.hauteur = hauteur;
+    t.largeur = largeur;
+    for (int i = 0; i < t.hauteur; ++i) {
+        t.chaines.push_back(nouvelle_chaine(this->index_aleatoire(), t.largeur));
+    }
+    sort(t.chaines.begin(), t.chaines.end(), compare2Chaine);
+    return t;
+}
+
+bool Inverter::write(string namefile) {
+    // Create and open a text file
+    ofstream MyFile(namefile);
+
+    if (!MyFile.good())
+        return false;
+
+    MyFile << this->hashingMethod << " " <<
+           this->alphabet << " " <<
+           this->minSize << " " <<
+           this->maxSize << " " <<
+           this->table.hauteur << " " <<
+           this->table.largeur << endl;
+
+
+    for (const auto &item: this->table.chaines) {
+
+        MyFile << item.b << endl << item.e << endl;
+    }
+    MyFile.close();
+    return true;
+}
+
+bool Inverter::read(string namefile) {
+
+
+    string str;
+    ifstream MyFile(namefile);
+
+    if (!MyFile.good())
+        return false;
+
+    getline(MyFile, str);
+
+    istringstream inverterInfo(str);
+
+    inverterInfo >> this->hashingMethod >>
+                 this->alphabet >>
+                 this->minSize >>
+                 this->maxSize >>
+                 this->table.hauteur >>
+                 this->table.largeur;
+
+
+    this->table.chaines = vector<Chaine>(this->table.hauteur);
+
+    uint64_t begin, end;
+    int n = 0;
+
+    while (MyFile >> this->table.chaines[n].b >> this->table.chaines[n].e) {
+        n++;
+    }
+
+    MyFile.close();
+    return true;
+}
+
+void Inverter::afficheTable(int shift) {
+    for (int i = 0; i < shift; ++i) {
+        cout << "Index : " << i << " : " <<
+             this->table.chaines[i].b << " ..... " <<
+             this->table.chaines[i].e << endl;
+    }
+    cout << "....." << endl << "....." << endl;
+    for (int i = shift; i > 0; --i) {
+        cout << "Index : " << this->table.chaines.size() - i << " : " <<
+             this->table.chaines[this->table.chaines.size() - i].b << " ..... " <<
+             this->table.chaines[this->table.chaines.size() - i].e << endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -10,13 +10,16 @@
 using namespace std;
 
 // Global vars
-Byte b[32];
+int size = 0;
+Byte *b;
 auto hashFunction = hash_MD5_string;
 int min_size = 4;
 int max_size = 5;
 string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 int value;
 int d = 5;
+string hashName;
+Inverter myInverter;
 
 string hexStr(Byte *data, int len) {
     stringstream ss;
@@ -61,13 +64,11 @@ void i2cTest(string s) {
     // Inverter myInverter3("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 4, 5, hashFunction);
     // cout << "When alphabet is ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz, min = 4 and max = 5 : " << myInverter3.i2c(i) << endl; 
 
-    Inverter myInverter(alphabet, min_size, max_size, hashFunction);
+    // Inverter myInverter(alphabet, min_size, max_size, hashFunction);
     cout << "When alphabet is "<< alphabet << ", min = "<< min_size << " and max = " << max_size << " : " << myInverter.i2c(i) << endl << endl;
 }
 
 void h2iTest(vector<string> args) {
-    Inverter myInverter(alphabet, min_size, max_size, hashFunction);    
-
     hash_MD5_string(args[0],b);
     cout << "alphabet = " << alphabet << endl << "taille_min = " << min_size << endl << "taille_max = " << max_size << endl << "N = " << myInverter.N << endl;
     cout << "MD5(\"oups\") = " << hexStr(b,32) << endl << "h2i(MD5(\"oups\"), "<< args[1] << ") = " << myInverter.h2i(b, stoi(args[1])) << endl << endl;
@@ -78,11 +79,40 @@ void i2iTest(vector<string> args) {
     // TODO: faire le test de i2i (QUESTION 6)
 }
 
+void rainbow_table(vector<string> args) {
+    myInverter.table = myInverter.creer_table(stoi(args[0]), stoi(args[1]));
+    myInverter.write(args[2]);
+}
+
+void rainbow_info(vector<string> args) {
+    myInverter.read(args[0]);
+    myInverter.afficheTable(stoi(args[1]));
+}
+
+void rainbow_stats(vector<string> args) {
+    
+}
+
+void rainbow_crack(vector<string> args) {
+    
+}
+
+void rainbow_bruteforce(vector<string> args) {
+    
+}
+
 int main(int argc, char *argv[]) {
 
     // CLI PARSING
     CLI::App app{};
-
+    // Create subcommand (via options)
+    vector<string> createArgs, infoArgs, statsArgs, crackArgs, bruteforceArgs;
+    auto create = app.add_option("--create", createArgs, "create the corresponding rainbow tables (M=height, T=width)")->expected(3);
+    auto info = app.add_option("--info", infoArgs, "display some information about the table from given file")->expected(2);
+    auto stats = app.add_option("--stats", statsArgs, "give some information (cover, size) about rainbow tables without computing them (M=height, T=width)")->expected(2);
+    auto crack = app.add_option("--crack", crackArgs, "crack the given hash with the rainbow tables")->allow_extra_args(true);
+    auto bruteforce = app.add_option("--bruteforce", bruteforceArgs, "brute force the given hash")->expected(1);
+    
     // App command flags
     bool isMD5{false};
     bool isSHA1{false};
@@ -91,15 +121,48 @@ int main(int argc, char *argv[]) {
 
     // App options
     app.add_option("--alphabet", alphabet, "allowed letters for cleartext");
-
     app.add_option("--min-size", min_size, "minimum size of clear text");
     app.add_option("--max-size", max_size, "maximum size of clear text");
-
     app.add_option("--value", value, "value that will be treated");
-
     app.add_option("-d, --delay", d, "number of seconds between consecutive log messages (default 5)");
 
-    // Test command
+    // Callback for main command
+    app.callback([&]() {
+
+        if (isMD5){
+            hashName = "MD5";
+            hashFunction = hash_MD5_string;
+            b = new Byte[16];
+            size = 16;
+        }
+        if (isSHA1){
+            hashFunction = hash_SHA1_string;
+            hashName = "SHA1";
+            b = new Byte[20];
+            size = 20;
+        }
+
+        string method;
+        if (hashFunction == hash_MD5_string)
+            method = "MD5";
+        if (hashFunction == hash_SHA1_string)
+            method = "SHA1";
+        
+        myInverter = Inverter(alphabet, min_size, max_size, hashFunction, size, method);
+        
+        if (!createArgs.empty())
+            rainbow_table(createArgs);
+        if (!infoArgs.empty())
+            rainbow_info(infoArgs);
+        if (!statsArgs.empty())
+            rainbow_stats(statsArgs);
+        if (!crackArgs.empty())
+            rainbow_crack(crackArgs);
+        if (!bruteforceArgs.empty())
+            rainbow_bruteforce(bruteforceArgs);
+    });
+
+    // Test subcommand
     bool isConfig = false;
     vector<string> sTab;
     vector<string> iTab;
@@ -107,6 +170,7 @@ int main(int argc, char *argv[]) {
     vector<string> i2iArgs;
     auto test = app.add_subcommand("test", "development tests (\"./main test --list\" for available tests)");
 
+    // Test flag and options
     test->add_flag("--config", isConfig, "print the default config for the tests");
     test->add_option("--hash", sTab, "compute hash of string s1, s2, ...");
     test->add_option("--i2c", iTab, "compute i2c(i1), i2c(i2), ...");
@@ -117,22 +181,17 @@ int main(int argc, char *argv[]) {
     // test->add_option("--FULL-chain", iTab, "compute (full, with details) chains starting at i1, i2, ...");
     // test->add_option("--chain", iTab, "compute chains starting at i1, i2, ...");
     // test->add_option("--search", iTab, "search the first and last occurences of i in table");
-    test->add_option("--list", "this list");
+    test->add_option("--list", "this list")->expected(0);
 
-    test->callback( [&](){
+    // Callback for test subcommand
+    test->callback([&]() {
         if (isConfig) {
             cout << "CONFIG : \n" << endl;
-            cout << "Byte b[32]" << endl << "hashFunction = hash_MD5_string"<< endl << "min_size = 4" << endl << "max_size = 5" << endl
-                << "alphabet = \"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\"" << endl << "d = 5" << endl;
-        } else {
-            std::cout << "Testing : \n\n";
+            cout << "Byte b[32]" << endl << "hashFunction = hash_MD5_string" << endl << "min_size = 4" << endl
+                 << "max_size = 5" << endl
+                 << "alphabet = \"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\"" << endl << "d = 5" << endl;
         }
 
-        if (isMD5)
-            hashFunction = hash_MD5_string;
-        if (isSHA1)
-            hashFunction = hash_SHA1_string;
-        
         if (!sTab.empty())
             for (int i = 0; i < sTab.size(); i++)
                 hashTest(sTab[i]);
@@ -149,9 +208,47 @@ int main(int argc, char *argv[]) {
     });
 
     // PARSING argv
-    Byte b[32];
-
     CLI11_PARSE(app, argc, argv);
 
     return 0;
 }
+
+// int main2(int argc, char *argv[]) {
+
+//     string s = "Salut";
+//     string s2 = "Bob";
+
+//     Inverter myInverter1("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 1, 3, hash_SHA1_string, 20, "MD5");
+//     Inverter myInverter2("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 4, 4, hash_SHA1_string, 20, "MD5");
+//     Inverter myInverter3("abcdefghijklmnopqrstuvwxyz", 4, 5, hash_MD5_string, 20, "MD5");
+
+//     hash_MD5_string(s, myInverter3.b);
+//     string res = hexStr(myInverter3.b, myInverter3.sizeByte);
+//     cout << res << endl;
+
+//     hash_SHA1_string(s2, myInverter3.b);
+//     res = hexStr(myInverter3.b, myInverter3.sizeByte);
+//     cout << res << endl;
+
+//     cout << myInverter1.i2c(12345) << endl;
+//     cout << myInverter2.i2c(1234) << endl;
+//     cout << myInverter3.i2c(382537153) << endl;
+
+//     hash_MD5_string("oups", myInverter3.b);
+//     cout << hexStr(myInverter3.b, myInverter3.sizeByte) << endl <<
+//          myInverter3.N << endl
+//          << "h2i : " << myInverter3.h2i(myInverter3.b, 1) << endl;
+
+//     cout << myInverter3.i2i(1, 1) << endl;
+
+//     Chaine chaine = myInverter3.nouvelle_chaine(1, 1);
+//     cout << "begin de taille 1 a " << 1 << ":" << chaine.b << " ..... " << chaine.e << endl;
+//     chaine = myInverter3.nouvelle_chaine(1, 2);
+//     cout << "begin de taille 1 a " << 2 << ":" << chaine.b << " ..... " << chaine.e << endl;
+//     chaine = myInverter3.nouvelle_chaine(1, 10);
+//     cout << "begin de taille 1 a " << 10 << ":" << chaine.b << " ..... " << chaine.e << endl;
+//     chaine = myInverter3.nouvelle_chaine(1, 100);
+//     cout << "begin de taille 1 a " << 100 << ":" << chaine.b << " ..... " << chaine.e << endl;
+
+//     return 0;
+// }
